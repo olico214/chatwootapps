d471f6c195a58c5a93934f9c1d3fe27d4fdf0fb6
olico214 <oliverromero73@gmail.com>
2025-11-17 16:50:16 -0600

    cambios

--- src/libs/mysql.js
+++ src/libs/mysql.js
@@ -5,16 +5,12 @@ let pool;
 
 // dotenv.config({ path: '.env.locale' });
 
-console.log("Conectando a la base de datos con los siguientes parámetros:");
-console.log("Usuario:", process.env.DB_USER);
-console.log("Servidor:", process.env.DB_SERVER);
-console.log("Base de datos:", process.env.DB_NAME);
 
 try {
   pool = mysql.createPool({
     user: "u835880732_chatwootapps",
-    password: "Y0SQE~i~",
-    server: "srv650.hstgr.io",
+    password: "P*aXvWvu&2",
+    host: "srv650.hstgr.io",
     database: "u835880732_chatwootapps",
     multipleStatements: true,
     namedPlaceholders: true,
